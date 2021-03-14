110fb2a5c853838ce397e15be4662a09e0053e9f
joselito9129@gmail.com <joselito9129@gmail.com>
2021-03-14 13:43:08 -0600

    Palindromo

---
+++ Palindromo/src/com/jn/second/Palindromo.java
@@ -0,0 +1,39 @@
+package com.jn.second;
+
+import java.util.Scanner;
+
+public class Palindromo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Por favor introduzca una palabra o frase con el teclado:");
+
+		String keyboardInput = "";
+		@SuppressWarnings("resource")
+		Scanner scannerInput = new Scanner(System.in);
+		keyboardInput = scannerInput.nextLine();
+
+		if (keyboardInput.isEmpty()) {
+			System.out.println("El texto no puede estar vacio.");
+
+		} else {
+			if (validPalindrome(keyboardInput)) {
+				System.out.println("La palabra o frase: " + keyboardInput + " es un Palindromo");
+			} else {
+				System.out.println("La palabra o frase: " + keyboardInput + " no es un Palindromo");
+			}
+		}
+
+	}
+
+	public static boolean validPalindrome(String text) {
+
+		text = text.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+				.replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
+
+		String inverted = new StringBuilder(text).reverse().toString();
+
+		return inverted.equals(text);
+	}
+
+}
